da47f334afa126d2fa9aacd67088c4d28c86d2c7
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-01-19 23:51:29 +0530

    updating country dataset according to year entered

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import plotly.express as px
 df = px.data.gapminder()
 
 st.write(df)
+
+year_options = df['year'].unique().tolist()
+year = st.selectbox('Which year would you like  to see?', year_options,0)
+df = df[df['year']==year]
