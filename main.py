87649c2562ea85ef20ae5c8dbc45f6b2c2d431a9
Nitin Pilkhwal <1nasty.np@gmail.com>
2023-01-20 00:02:48 +0530

    adding scattering graph

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ st.write(df)
 year_options = df['year'].unique().tolist()
 year = st.selectbox('Which year would you like  to see?', year_options,0)
 df = df[df['year']==year]
+
+fig = px.scatter(df,x="gdpPercap",y="lifeExp",size="pop", color="continent",hover_name="continent",log_x=True, size_max=55, range_x=[100,100000],range_y=[25,90])
+
+fig.update_layout(width=800)
+
+st.write(fig)
